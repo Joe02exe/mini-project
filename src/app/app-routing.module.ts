4eb92ee549b546f82d41237342210ec38fac94de
Joe02exe <lonza.johannes@gmail.com>
2023-07-14 12:06:10 +0200

    added basic "mock" -authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { DragDropTaskComponent } from './drag-drop-task/drag-drop-task.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [  
-  { path: 'dashboard', component: DragDropTaskComponent },
+  { path: 'dashboard', canActivate: [AuthGuard], component: DragDropTaskComponent },
   { path: 'register', component: RegisterComponent},
   { path: 'login', component: LoginComponent }
 
